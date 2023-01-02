826898308f7085ecb38bc6bf6974f24bf5feac5d
burak <burakaydin9761@gmail.com>
2023-01-02 09:25:55 +0300

    initial commit

---
+++ week-4-homework-2/week-4-homework-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week-4-homework-2
+//
+//  Created by Burak on 2.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
